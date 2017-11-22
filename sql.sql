3ba113d8da115fab76aeae6f56806d13168c3f8a
AndreVictor60 <andre.victor60@free.fr>
2017-11-22 08:41:20 +0100

    fini

---
+++ sql.sql
@@ -0,0 +1,28 @@
+Exercice 1:
+
+select * from languages;
+
+Exercice 2:
+
+select * from languages where language='PHP';
+
+Exercice 3:
+
+select * from languages where language='PHP' or language='JavaScript';
+
+Exercice 4: 
+
+select * from languages where id=3 or id=5 or id=7;
+
+Exercice 5:
+
+select * from languages where language='JavaScript' order by id limit 2 offset 0;
+
+Exercice 6:
+
+select * from languages where not language='PHP';
+
+Exercice 7:
+
+select * from languages order by language asc;
+
